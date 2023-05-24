446f62a7278b1ac020b59b0e3016082738e35952
Ronak Pal Singh Bali <ronaqpalsinghbali@gmail.com>
2023-05-24 15:58:15 +0530

    Added first workflow with EC2 file

---
+++ .github/workflows/EC2.tf
@@ -0,0 +1,30 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+resource "aws_instance" "ronak-EC2" {
+
+  instance_type = "t2-micro"
+  ami           = "ami-0d86c69530d0a048e"
+
+  tags        = var.tags_ronak
+  volume_tags = var.tags_ronak
+
+
+}
+
+
+variable "tags_ronak" {
+
+  type = map(any)
+  default = {
+    Name : "Ronak"
+    Owner : "ronak.bali@cloudeq.com"
+    Purpose : "Training"
+  }
+
+}
