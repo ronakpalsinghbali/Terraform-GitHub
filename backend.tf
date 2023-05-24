4664edbf13249bf298de4cb65d503c49fe4a7b8a
Ronak Pal Singh Bali <ronaqpalsinghbali@gmail.com>
2023-05-24 18:46:28 +0530

    modified backend file and WF files

--- backend.tf
+++ backend.tf
@@ -5,3 +5,11 @@ terraform {
     region = "us-east-1"
   }
 }
+
+
+
+# This is for use in command line
+# terraform init \
+#     -backend-config="bucket= mybucket-1410" \
+#     -backend-config="key= ronak-tfstate" \
+#     -backend-config="region= us-east-1"
