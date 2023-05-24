9b885b3530599dc4da54e23d0bbfa457afa8d203
Ronak Pal Singh Bali <ronaqpalsinghbali@gmail.com>
2023-05-24 16:16:42 +0530

    Added subnet id to ec2 file

--- EC2.tf
+++ EC2.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "ronak-EC2" {
 
   instance_type = "t2.micro"
   ami           = "ami-0d86c69530d0a048e"
+  subnet_id     = "subnet-02fd86ec22e3fac87"
 
   tags        = var.tags_ronak
   volume_tags = var.tags_ronak
